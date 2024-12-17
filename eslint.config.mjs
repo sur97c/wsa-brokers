03a52ec096498ad807fe772d46df20b05f262e2b
Luis Antonio Vázquez Ramos <lvazquez@totaltech.com.mx>
2024-12-17 10:57:22 -0600

    Added lint, husky, prettier rules and configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,48 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+import * as path from 'path'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+import pluginTypeScript from '@typescript-eslint/eslint-plugin'
+import parserTypeScript from '@typescript-eslint/parser'
+import pluginPrettier from 'eslint-plugin-prettier'
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: path.resolve('./'), // Asegura que las rutas sean correctas.
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'], // Define los archivos que deben ser validados.
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parser: parserTypeScript,
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      '@typescript-eslint': pluginTypeScript,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'react/prop-types': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  ...compat.config({
+    extends: [
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+      'next',
+      'next/core-web-vitals',
+    ],
+  }),
+]
