f94ba369b18a1941b89aaa19de371dc21968e56f
Luis Antonio Vázquez Ramos <lvazquez@totaltech.com.mx>
2025-01-07 01:04:53 -0600

    Added BrokerDashboard component

--- app/[lang]/(protected)/app/dashboard/page.tsx
+++ app/[lang]/(protected)/app/dashboard/page.tsx
@@ -3,13 +3,17 @@
 'use client'
 
 import { useTranslations } from '@/translations/hooks/useTranslations'
+import BrokerDashboard from './BrokerDashboard'
 
 export default function Management() {
   const { t, translations } = useTranslations()
 
   return (
-    <h1 className="text-2xl font-bold mb-4">
-      {t(translations.modules.dashboard.title)}
-    </h1>
+    <>
+      <h1 className="text-2xl font-bold mb-4">
+        {t(translations.modules.dashboard.title)}
+      </h1>
+      <BrokerDashboard />
+    </>
   )
 }
